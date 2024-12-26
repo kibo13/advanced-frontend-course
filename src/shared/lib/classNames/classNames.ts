5c048809f7a7e40c6806fc7d402318893c6c1799
kibo13 <kimboris1310@gmail.com>
2024-12-26 15:36:51 +0500

    chore: setup eslint for TypeScript and fix linting errors

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -9,7 +9,7 @@ export const classNames = (
 		cls,
 		...additional.filter(Boolean),
 		...Object.entries(mods)
-			.filter(([className, value]) => Boolean(value))
-			.map(([className, value]) => className),
+			.filter(([_, value]) => Boolean(value))
+			.map(([className, _]) => className),
 	].join(' ')
 }
