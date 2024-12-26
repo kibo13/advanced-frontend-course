1d9283cf83be6dacb0413de59e863b728a3d6cff
kibo13 <kimboris1310@gmail.com>
2024-12-26 17:31:22 +0500

    temp: remove translations from suspense

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,15 +5,13 @@ import { Sidebar } from 'widgets/Sidebar'
 import { classNames } from 'shared/lib/classNames/classNames'
 import { Suspense } from 'react'
 import 'app/styles/index.scss'
-import { useTranslation } from 'react-i18next'
 
 const App = () => {
 	const { theme } = useTheme()
-	const { t } = useTranslation('translation')
 
 	return (
 		<div className={classNames('app', {}, [theme])}>
-			<Suspense fallback={<div>{t('Loading')}</div>}>
+			<Suspense fallback={<div>Loading...</div>}>
 				<Navbar />
 				<div className="content-page">
 					<Sidebar />
--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,19 +1,17 @@
 import { Suspense } from 'react'
-import { useTranslation } from 'react-i18next'
 import { Route, Routes } from 'react-router-dom'
 import { routeConfig } from 'shared/config/routeConfig/routeConfig'
 
 const AppRouter = () => {
-	const { t } = useTranslation('translation')
 	return (
-		<Suspense fallback={<div>{t('Loading')}</div>}>
+		<Suspense fallback={<div>Loading...</div>}>
 			<Routes>
 				{Object.values(routeConfig).map(({ path, element }) => (
 					<Route
 						key={path}
 						path={path}
 						element={
-							<Suspense fallback={<div>{t('Loading')}</div>}>
+							<Suspense fallback={<div>Loading...</div>}>
 								<div className="page-wrapper">{element}</div>
 							</Suspense>
 						}
