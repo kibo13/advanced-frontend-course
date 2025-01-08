70387690fe7c6bfefadd9f324abda9891d70fa11
kibo13 <kimboris1310@gmail.com>
2025-01-08 17:08:14 +0500

    feat(test): enhance componentRender with StoreProvider and initialState

--- src/shared/lib/tests/componentRender/componentRender.tsx
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -1,22 +1,27 @@
 import { render } from '@testing-library/react'
+import { StateSchema, StoreProvider } from 'app/providers/StoreProvider'
 import { ReactNode } from 'react'
 import { I18nextProvider } from 'react-i18next'
 import { MemoryRouter } from 'react-router-dom'
+import { DeepPartial } from '@reduxjs/toolkit'
 import i18nextForTests from 'shared/config/i18n/i18nForTests'
 
 export interface ComponentRenderOptions {
 	route?: string
+	initialState?: DeepPartial<StateSchema>
 }
 
 export const componentRender = (
 	component: ReactNode,
 	options: ComponentRenderOptions = {}
 ) => {
-	const { route = '/' } = options
+	const { route = '/', initialState } = options
 
 	return render(
-		<MemoryRouter initialEntries={[route]}>
-			<I18nextProvider i18n={i18nextForTests}>{component}</I18nextProvider>
-		</MemoryRouter>
+		<StoreProvider initialState={initialState}>
+			<MemoryRouter initialEntries={[route]}>
+				<I18nextProvider i18n={i18nextForTests}>{component}</I18nextProvider>
+			</MemoryRouter>
+		</StoreProvider>
 	)
 }
