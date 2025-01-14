9f7594d65852fc2d3b27f58e08c4712d0bc12e00
kibo13 <kimboris1310@gmail.com>
2025-01-14 15:55:36 +0500

    test: fix loginByUsername test

--- src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.test.ts
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.test.ts
@@ -1,18 +1,13 @@
-import axios from 'axios'
-import { loginByUsername } from './loginByUsername'
 import { userActions } from 'entities/User'
 import { TestAsyncThunk } from 'shared/lib/tests/TestAsyncThunk/TestAsyncThunk'
-
-jest.mock('axios')
-
-const mockedAxios = jest.mocked(axios, true)
+import { loginByUsername } from './loginByUsername'
 
 describe('loginByUsername.test', () => {
 	test('dispatches setUserData and returns fulfilled on successful login', async () => {
-		const userValue = { username: '123', id: '1' }
-		mockedAxios.post.mockReturnValue(Promise.resolve({ data: userValue }))
+		const userValue = { username: 'admin', id: '1' }
 
 		const thunk = new TestAsyncThunk(loginByUsername)
+		thunk.api.post.mockReturnValue(Promise.resolve({ data: userValue }))
 		const result = await thunk.callThunk({
 			username: 'admin',
 			password: 'secret',
@@ -22,22 +17,21 @@ describe('loginByUsername.test', () => {
 			userActions.setUserData(userValue)
 		)
 		expect(thunk.dispatch).toHaveBeenCalledTimes(3)
-		expect(mockedAxios.post).toHaveBeenCalled()
+		expect(thunk.api.post).toHaveBeenCalled()
 		expect(result.meta.requestStatus).toBe('fulfilled')
-		expect(result.payload).toBe(userValue)
+		expect(result.payload).toEqual(userValue)
 	})
 
 	test('returns rejected on failed login attempt', async () => {
-		mockedAxios.post.mockReturnValue(Promise.resolve({ status: 403 }))
-
 		const thunk = new TestAsyncThunk(loginByUsername)
+		thunk.api.post.mockReturnValue(Promise.resolve({ status: 403 }))
 		const result = await thunk.callThunk({
 			username: 'admin',
 			password: 'secret',
 		})
 
 		expect(thunk.dispatch).toHaveBeenCalledTimes(2)
-		expect(mockedAxios.post).toHaveBeenCalled()
+		expect(thunk.api.post).toHaveBeenCalled()
 		expect(result.meta.requestStatus).toBe('rejected')
 		expect(result.payload).toBe('error')
 	})
